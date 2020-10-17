7c9fccc239e487a12052a81f7ddb2701e28d3f45
unknown <akashngadhave.com>
2020-10-17 13:48:54 +0530

    Added 3 new files,background color is changed after clicking on button

---
+++ Change bg color/changebg.js
@@ -0,0 +1,15 @@
+function red() {
+	document.querySelector('body').style.backgroundColor="red";
+}
+
+function green() {
+	document.querySelector('body').style.backgroundColor="green";
+}
+
+function yellow() {
+	document.querySelector('body').style.backgroundColor="yellow";
+}
+
+function blue() {
+	document.querySelector('body').style.backgroundColor="blue";
+}
